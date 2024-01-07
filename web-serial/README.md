a88d5f8aa8bd21f44450e751c08165991a408548
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-01-07 11:11:21 -0800

    Web serial send input

--- web-serial/README.md
+++ web-serial/README.md
@@ -1,10 +1,10 @@
 # Web Serial
-View `println!()`s with your browser
+Read and write to a serial port on a web page. You can use this to communicate with an ESP32-C3 without installing anything, just by visiting a web page. It even works on Chromebooks without any special setup. In fact, it works even better with ChromeOS than normal Linux distros.
 
 ## Instructions
 - Flash a program that prints to output on your ESP32-C3
 - Connect the ESP32-C3 to your computer
-- Make sure you can access `/dev/ttyACM0` in Linux
+- Make sure you can access `/dev/ttyACM0` if you're using Linux
 - Open the web page using a Chromium-based browser. A demo is available at https://serial-output.netlify.app/
 
 ## Resources
--- web-serial/src/App.tsx
+++ web-serial/src/App.tsx
@@ -1,32 +1,73 @@
-import { FC, useState } from 'react';
+import { FC, useRef, useState } from 'react';
 import { Helmet } from 'react-helmet'
 import './style.css';
 
+interface PortData {
+  port: SerialPort,
+  output: Uint8Array
+}
+
 export const App: FC<{ name: string }> = ({ name }) => {
-  const [output, setOutput] = useState<Uint8Array>()
+  const [portData, setPortData] = useState<PortData>()
+  const [input, setInput] = useState('')
+  const logRef = useRef<HTMLPreElement>(null)
 
   return (
     <>
       <Helmet>
         <title>Web Serial Viewer</title>
       </Helmet>
-      <button onClick={async () => {
-        const port = await navigator.serial.requestPort()
-        await port.open({ baudRate: 460800 })
-        const reader = port.readable.getReader()
-        while (true) {
-          const { value, done } = await reader.read();
-          if (done) {
-            // |reader| has been canceled.
-            break;
-          }
-          setOutput(output => output === undefined ? value : new Uint8Array([...output, ...value]))
-          // Hacks
-          document.body.scrollTo(0, Number.MAX_SAFE_INTEGER)
-        }
-      }}>Connect to ESP32-C3 (or something else, doesn't really have to be an ESP32-C3)</button><br />
-      Output:<br />
-      <pre><code>{output !== undefined ? new TextDecoder().decode(output) : <i>No output yet</i>}</code></pre>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <button
+          disabled={portData !== undefined}
+          onClick={async () => {
+            const port = await navigator.serial.requestPort()
+            await port.open({ baudRate: 460800 })
+            setPortData({
+              port,
+              output: new Uint8Array()
+            })
+            const reader = port.readable.getReader()
+            while (true) {
+              const { value, done } = await reader.read();
+              if (done) {
+                // |reader| has been canceled.
+                break;
+              }
+              setPortData(data => ({
+                ...data,
+                output: new Uint8Array([...data.output, ...value])
+              }))
+
+              // Scroll to bottom
+              logRef.current.scrollTo(0, Number.MAX_SAFE_INTEGER)
+            }
+          }}
+        >
+          {portData === undefined ? 'Connect' : 'Connected'}
+        </button><br />
+
+        <form
+          onSubmit={async e => {
+            e.preventDefault()
+            console.log(input)
+            setInput('')
+
+            const writer = portData.port.writable.getWriter()
+            await writer.write(new TextEncoder().encode(`${input}\n`))
+            writer.releaseLock()
+          }}
+        >
+          <label>
+            Send line of input <br />
+            <input style={{ fontFamily: 'monospace' }} value={input} onChange={({ target: { value } }) => setInput(value)} />
+            <button type='submit' disabled={portData === undefined}>Send</button>
+          </label>
+        </form>
+
+        Output:<br />
+        <pre ref={logRef} style={{ flexGrow: 1, overflow: 'auto' }}><code>{portData !== undefined ? new TextDecoder().decode(portData.output) : <i>No output yet</i>}</code></pre>
+      </div>
     </>
   );
 };
