4d1fa655c85049668a4384af9c03187f031dc8f6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-01-07 11:43:33 -0800

    Disable watchdog in echo example

--- usb-echo/src/main.rs
+++ usb-echo/src/main.rs
@@ -1,11 +1,16 @@
 use esp_idf_sys as _;
-use std::thread;
-use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // It is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
+
+    // Disable watchdog
+    unsafe {
+        esp_idf_sys::esp_task_wdt_delete(esp_idf_sys::xTaskGetIdleTaskHandleForCPU(
+            esp_idf_hal::cpu::core() as u32,
+        ))
+    };
 
     let mut buf = [0u8; 1024];
     let buf_ptr = buf.as_mut_ptr() as *mut libc::c_void;
@@ -15,6 +20,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if len > 0 {
             unsafe { libc::write(libc::STDOUT_FILENO, buf_ptr, len as usize) };
         }
-        thread::sleep(Duration::from_millis(10));
     }
 }
