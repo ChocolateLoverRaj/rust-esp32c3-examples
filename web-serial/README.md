63ac92a08f1c2b94d8dff504d750fc87ed2d7ece
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-01-07 09:42:38 -0800

    Web Serial

---
+++ web-serial/README.md
@@ -0,0 +1,12 @@
+# Web Serial
+View `println!()`s with your browser
+
+## Instructions
+- Flash a program that prints to output on your ESP32-C3
+- Connect the ESP32-C3 to your computer
+- Make sure you can access `/dev/ttyACM0` in Linux
+- Open the web page using a Chromium-based browser
+
+## Resources
+- https://developer.mozilla.org/en-US/docs/Web/API/Web_Serial_API
+- https://docs.espressif.com/projects/esp-idf/en/latest/esp32c3/get-started/establish-serial-connection.html
---
+++ web-serial/src/App.tsx
@@ -0,0 +1,29 @@
+import { FC, useState } from 'react';
+
+import './style.css';
+
+export const App: FC<{ name: string }> = ({ name }) => {
+  const [output, setOutput] = useState<Uint8Array>()
+
+  return (
+    <>
+      <button onClick={async () => {
+        const port = await navigator.serial.requestPort()
+        await port.open({ baudRate: 460800 })
+        const reader = port.readable.getReader()
+        while (true) {
+          const { value, done } = await reader.read();
+          if (done) {
+            // |reader| has been canceled.
+            break;
+          }
+          setOutput(output => output === undefined ? value : new Uint8Array([...output, ...value]))
+          // Hacks
+          document.body.scrollTo(0, Number.MAX_SAFE_INTEGER)
+        }
+      }}>Connect to ESP32-C3</button><br />
+      Output:<br />
+      <pre><code>{output !== undefined ? new TextDecoder().decode(output) : <i>No output yet</i>}</code></pre>
+    </>
+  );
+};
