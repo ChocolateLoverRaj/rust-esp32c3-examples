df8a434845956592a7257b2998bdc323159ca8f2
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-06-08 10:50:39 -0700

    FHS Flake

--- flake.nix
+++ flake.nix
@@ -1,56 +1,40 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    esp-dev = {
-      url = "github:thiskappaisgrey/nixpkgs-esp-dev-rust";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , esp-dev
-    ,
-    }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ esp-dev.overlay ];
-      };
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          gcc-riscv32-esp32c3-elf-bin
-          openocd-esp32-bin
-
-          # Tools required to use ESP-IDF.
-          git
-          wget
-          gnumake
-
-          flex
-          bison
-          gperf
-          pkg-config
-
-          ninja
-          libclang
-
-          ncurses5
-
-          python3
-          python3Packages.pip
-          python3Packages.virtualenv
-          libudev-zero
-          ldproxy
-        ];
-
-        shellHook = ''
-          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.libxml2 pkgs.zlib pkgs.stdenv.cc.cc.lib ]}"
-          export LIBCLANG_PATH=${pkgs.libclang.lib}/lib
-        '';
-      };
+  outputs = {
+    self,
+    nixpkgs
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
     };
+    fhs = pkgs.buildFHSUserEnv {
+      name = "fhs-shell";
+      targetPkgs = pkgs: with pkgs; [
+        gcc
+
+        pkg-config
+        libclang.lib
+        gnumake
+        cmake
+        ninja
+
+        git
+        wget
+
+        rustup
+        cargo-generate
+
+        espflash
+        python3
+        python3Packages.pip
+        python3Packages.virtualenv
+	ldproxy
+      ];
+    };
+  in {
+    devShells.${pkgs.system}.default = fhs.env;
+  };
 }
