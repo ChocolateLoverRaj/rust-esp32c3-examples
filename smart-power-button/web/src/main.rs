9adf37af92dc96b1e2ebecd14e3e056f6e440e06
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-07-05 09:27:27 -0700

    Use WSS for HTTPS

--- smart-power-button/web/src/main.rs
+++ smart-power-button/web/src/main.rs
@@ -1,7 +1,9 @@
-use async_ui_web::{join, mount};
+use std::panic;
+
 use async_ui_web::event_traits::EmitElementEvent;
 use async_ui_web::html::{Br, Button};
 use async_ui_web::shortcut_traits::{ShortcutRenderStr, UiFutureExt};
+use async_ui_web::{join, mount};
 use dotenvy_macro::option_dotenv;
 use futures::{SinkExt, StreamExt};
 use gloo_console::{error, log};
@@ -15,27 +17,36 @@ use smart_power_button_common::{MessageToEsp, MessageToWeb};
 
 use crate::stream_render_ext::StreamRenderExt;
 
-mod web_socket_ext;
 mod stream_render_ext;
+mod web_socket_ext;
 
 fn main() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
     mount(app());
 }
 
 async fn app() {
     let ws_host = option_dotenv!("WS_HOST")
-        .map_or(window().unwrap().location().host().unwrap(), |s: &str| s.to_owned());
-    let ws_url = format!("ws://{ws_host}");
-    match WsMeta::connect(ws_url, None).meanwhile("Opening web socket".render()).await {
+        .map_or(window().unwrap().location().host().unwrap(), |s: &str| {
+            s.to_owned()
+        });
+    let ws_protocol = match window().unwrap().location().protocol().unwrap().as_str() {
+        "http" => "ws",
+        "https" => "wss",
+        _ => unreachable!("Unknown protocol"),
+    };
+    let ws_url = format!("${ws_protocol}://{ws_host}");
+    match WsMeta::connect(ws_url, None)
+        .meanwhile("Opening web socket".render())
+        .await
+    {
         Ok((_ws_meta, ws_stream)) => {
             let (w, r) = ws_stream.split();
             let w = Mutex::new(w);
             let message_to_web_stream = r
                 .map(|message| match message {
-                    WsMessage::Binary(data) => {
-                        postcard::from_bytes::<MessageToWeb>(&data).unwrap()
-                    }
-                    WsMessage::Text(_) => unreachable!()
+                    WsMessage::Binary(data) => postcard::from_bytes::<MessageToWeb>(&data).unwrap(),
+                    WsMessage::Text(_) => unreachable!(),
                 })
                 // This line is just to debug messages
                 .inspect(|message| log!(format!("{message:?}")))
@@ -46,31 +57,41 @@ async fn app() {
                 "Power LED Status: ".render(),
                 message_to_web_stream
                     .clone()
-                    .filter_map(|(_, message)| Box::pin(async move {
-                        match message {
-                            MessageToWeb::PowerLedStatus(is_on) => Some(is_on),
-                            _ => None
+                    .filter_map(|(_, message)| {
+                        Box::pin(async move {
+                            match message {
+                                MessageToWeb::PowerLedStatus(is_on) => Some(is_on),
+                                _ => None,
+                            }
+                        })
+                    })
+                    .map(|is_on| {
+                        match is_on {
+                            true => "On",
+                            false => "Off",
                         }
-                    }))
-                    .map(|is_on| match is_on {
-                        true => "On",
-                        false => "Off"
-                    }.render())
+                        .render()
+                    })
                     .render(),
                 Br::new().render(),
                 "HDD LED Status: ".render(),
                 message_to_web_stream
                     .clone()
-                    .filter_map(|(_, message)| Box::pin(async move {
-                        match message {
-                            MessageToWeb::HddLedStatus(is_on) => Some(is_on),
-                            _ => None
+                    .filter_map(|(_, message)| {
+                        Box::pin(async move {
+                            match message {
+                                MessageToWeb::HddLedStatus(is_on) => Some(is_on),
+                                _ => None,
+                            }
+                        })
+                    })
+                    .map(|is_on| {
+                        match is_on {
+                            true => "On",
+                            false => "Off",
                         }
-                    }))
-                    .map(|is_on| match is_on {
-                        true => "On",
-                        false => "Off"
-                    }.render())
+                        .render()
+                    })
                     .render(),
                 Br::new().render(),
                 async {
@@ -81,14 +102,15 @@ async fn app() {
                         Br::new().render(),
                         long_press_button.render("Press power button for a long time".render()),
                         async {
-                            let mut stream = message_to_web_stream
-                                .clone()
-                                .filter_map(|(_, message)| Box::pin(async move {
-                                    match message {
-                                        MessageToWeb::PowerButtonStatus(is_on) => Some(is_on),
-                                        _ => None
-                                    }
-                                }));
+                            let mut stream =
+                                message_to_web_stream.clone().filter_map(|(_, message)| {
+                                    Box::pin(async move {
+                                        match message {
+                                            MessageToWeb::PowerButtonStatus(is_on) => Some(is_on),
+                                            _ => None,
+                                        }
+                                    })
+                                });
                             while let Some(is_on) = stream.next().await {
                                 short_press_button.set_disabled(is_on);
                                 long_press_button.set_disabled(is_on);
@@ -99,7 +121,13 @@ async fn app() {
                                 short_press_button.until_click().await;
                                 short_press_button.set_disabled(true);
                                 long_press_button.set_disabled(true);
-                                w.lock().await.send(WsMessage::Binary(to_allocvec(&MessageToEsp::ShortPressPowerButton).unwrap())).await.unwrap();
+                                w.lock()
+                                    .await
+                                    .send(WsMessage::Binary(
+                                        to_allocvec(&MessageToEsp::ShortPressPowerButton).unwrap(),
+                                    ))
+                                    .await
+                                    .unwrap();
                             }
                         },
                         async {
@@ -107,10 +135,17 @@ async fn app() {
                                 long_press_button.until_click().await;
                                 short_press_button.set_disabled(true);
                                 long_press_button.set_disabled(true);
-                                w.lock().await.send(WsMessage::Binary(to_allocvec(&MessageToEsp::LongPressPowerButton).unwrap())).await.unwrap();
+                                w.lock()
+                                    .await
+                                    .send(WsMessage::Binary(
+                                        to_allocvec(&MessageToEsp::LongPressPowerButton).unwrap(),
+                                    ))
+                                    .await
+                                    .unwrap();
                             }
-                        }
-                    )).await;
+                        },
+                    ))
+                    .await;
                 },
                 Br::new().render(),
                 async {
@@ -119,14 +154,15 @@ async fn app() {
                     join((
                         reset_button.render("Press reset button".render()),
                         async {
-                            let mut stream = message_to_web_stream
-                                .clone()
-                                .filter_map(|(_, message)| Box::pin(async move {
-                                    match message {
-                                        MessageToWeb::ResetButtonStatus(is_on) => Some(is_on),
-                                        _ => None
-                                    }
-                                }));
+                            let mut stream =
+                                message_to_web_stream.clone().filter_map(|(_, message)| {
+                                    Box::pin(async move {
+                                        match message {
+                                            MessageToWeb::ResetButtonStatus(is_on) => Some(is_on),
+                                            _ => None,
+                                        }
+                                    })
+                                });
                             while let Some(is_on) = stream.next().await {
                                 reset_button.set_disabled(is_on);
                             }
@@ -135,12 +171,20 @@ async fn app() {
                             loop {
                                 reset_button.until_click().await;
                                 reset_button.set_disabled(true);
-                                w.lock().await.send(WsMessage::Binary(to_allocvec(&MessageToEsp::ShortPressResetButton).unwrap())).await.unwrap();
+                                w.lock()
+                                    .await
+                                    .send(WsMessage::Binary(
+                                        to_allocvec(&MessageToEsp::ShortPressResetButton).unwrap(),
+                                    ))
+                                    .await
+                                    .unwrap();
                             }
-                        }
-                    )).await;
+                        },
+                    ))
+                    .await;
                 },
-            )).await;
+            ))
+            .await;
         }
         Err(e) => {
             error!(format!("{e:#?}"));
