49e5062d19533291a2d57eb14550e10fe485d038
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-08-03 13:59:37 -0700

    update

--- smart-power-button/computer/src/toggle_game_mode.rs
+++ smart-power-button/computer/src/toggle_game_mode.rs
@@ -7,7 +7,7 @@ use crate::samsung::Samsung;
 /// Toggles game mode, but doesn't exit the home menu after that
 pub async fn toggle_game_mode(remote: &mut Samsung) -> anyhow::Result<()> {
     remote.send_key("KEY_MENU").await?;
-    sleep(Duration::from_secs_f64(1.0)).await;
+    sleep(Duration::from_secs_f64(2.0)).await;
     for _ in 0..3 {
         remote.send_key("KEY_DOWN").await?;
         sleep(Duration::from_secs_f64(0.3)).await;
