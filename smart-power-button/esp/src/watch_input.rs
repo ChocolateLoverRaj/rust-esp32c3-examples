d3b3ddb6e2a244ddbab8da948020a3af2aebf1c6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-10-13 09:57:44 -0700

    Format watch_input.rs

--- smart-power-button/esp/src/watch_input.rs
+++ smart-power-button/esp/src/watch_input.rs
@@ -1,19 +1,27 @@
-use std::future::Future;
+use crate::value_channel::{value_channel, ValueReceiver};
 use esp_idf_svc::hal::gpio::{InputPin, InterruptType, OutputPin, PinDriver, Pull};
 use log::info;
-use crate::value_channel::{value_channel, ValueReceiver};
+use std::future::Future;
 
-pub fn watch_input<T: InputPin + OutputPin>(pin: T) -> anyhow::Result<(impl Future<Output=anyhow::Result<()>>, ValueReceiver<bool>)> {
+pub fn watch_input<T: InputPin + OutputPin>(
+    pin: T,
+) -> anyhow::Result<(
+    impl Future<Output = anyhow::Result<()>>,
+    ValueReceiver<bool>,
+)> {
     let mut pin = PinDriver::input(pin)?;
     pin.set_pull(Pull::Down).unwrap();
     pin.set_interrupt_type(InterruptType::AnyEdge).unwrap();
     pin.enable_interrupt().unwrap();
     let (tx, rx) = value_channel(pin.is_high());
-    Ok((async move {
-        loop {
-            pin.wait_for_any_edge().await?;
-            info!("Pin {} is: {:?}", pin.pin(), pin.get_level());
-            tx.update_if_changed(pin.is_high()).await;
-        }
-    }, rx))
+    Ok((
+        async move {
+            loop {
+                pin.wait_for_any_edge().await?;
+                info!("Pin {} is: {:?}", pin.pin(), pin.get_level());
+                tx.update_if_changed(pin.is_high()).await;
+            }
+        },
+        rx,
+    ))
 }
