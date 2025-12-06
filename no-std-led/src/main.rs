4afe773659f7ff3cd033873ee7392c1f8318cea6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-12-05 17:32:28 -0800

    Fixed it!

--- no-std-led/src/main.rs
+++ no-std-led/src/main.rs
@@ -5,7 +5,12 @@ use core::future::pending;
 
 use embassy_executor::Spawner;
 use esp_backtrace as _;
-use esp_println::println;
+use esp_hal::{
+    gpio::{Level, Output, OutputConfig},
+    interrupt::software::SoftwareInterruptControl,
+    timer::timg::TimerGroup,
+};
+use esp_println as _;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -13,9 +18,16 @@ esp_bootloader_esp_idf::esp_app_desc!();
 async fn main(spawner: Spawner) {
     let _ = spawner;
 
-    let _peripherals = esp_hal::init(Default::default());
+    let peripherals = esp_hal::init(Default::default());
 
-    println!("Hello cool Rust ESP32 no_std ecosystem!");
+    // Needed for esp_rtos
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let software_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, software_interrupt.software_interrupt0);
+
+    defmt::info!("Hello cool Rust ESP32 no_std ecosystem!");
+
+    let mut led = Output::new(peripherals.GPIO8, Level::Low, OutputConfig::default());
 
     pending().await
 }
