e53d829d12425936b14c2885112f040dedeb8ad1
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-12-16 14:35:28 -0800

    Add WS2812 example
    I will probably make all examples no_std and even convert the std ones to no_std in the future

--- flake.nix
+++ flake.nix
@@ -1,72 +1,37 @@
 {
   inputs = {
+    # Use espflash <4.0.0 until https://github.com/esp-rs/espflash/pull/975 is put into nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
-      self,
       nixpkgs,
       rust-overlay,
+      flake-utils,
+      ...
     }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [
-          (import rust-overlay)
-        ];
-      };
-    in
-    {
-      devShells.x86_64-linux.default =
-        with pkgs;
-        pkgs.mkShell {
-          buildInputs = [
-            # Tools required to use ESP-IDF.
-            git
-            wget
-            gnumake
-
-            flex
-            bison
-            gperf
-            pkg-config
-
-            ninja
-            libclang
-
-            ncurses5
-
-            python3
-            python3Packages.pip
-            python3Packages.virtualenv
-            libudev-zero
-            ldproxy
-
-            (rust-bin.selectLatestNightlyWith (
-              toolchain:
-              toolchain.default.override {
-                extensions = [ "rust-src" ];
-                targets = [ "wasm32-unknown-unknown" ];
-              }
-            ))
-            espflash
-            openssl
-            trunk
-            cargo-generate
-          ];
-
-          shellHook = ''
-            export LD_LIBRARY_PATH="${
-              lib.makeLibraryPath [
-                libxml2_13
-                zlib
-                stdenv.cc.cc.lib
-              ]
-            }"
-            export LIBCLANG_PATH=${libclang.lib}/lib
-          '';
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-    };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+              rust-analyzer
+              # Used by cargo run
+              espflash
+            ];
+          };
+      }
+    );
 }
