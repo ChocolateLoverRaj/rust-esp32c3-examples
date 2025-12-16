85afb3d0857825520b5dd3053433951a6530186a
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-12-16 11:58:46 -0800

    Add hello world example

---
+++ no_std_hello_world/src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use esp_backtrace as _;
+use esp_hal::{interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup};
+use esp_println as _;
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[esp_rtos::main]
+async fn main(spawner: Spawner) {
+    let _ = spawner;
+
+    let peripherals = esp_hal::init(Default::default());
+
+    // Needed for esp_rtos
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let software_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, software_interrupt.software_interrupt0);
+
+    info!("Hello from a Rust no_std environment with esp_rtos (basically embassy for ESP32).");
+}
