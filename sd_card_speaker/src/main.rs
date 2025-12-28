058e090f31d38072a659959bb2b915842535b705
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-12-27 18:31:06 -0800

    Start SD Card + Speaker example

---
+++ sd_card_speaker/src/main.rs
@@ -0,0 +1,219 @@
+#![no_std]
+#![no_main]
+use core::mem::transmute;
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
+use embassy_time::Delay;
+use esp_backtrace as _;
+use esp_hal::{
+    dma::{DmaRxBuf, DmaTxBuf},
+    dma_buffers, dma_circular_buffers,
+    gpio::{Level, Output, OutputConfig},
+    i2s::{
+        self,
+        master::{DataFormat, I2s},
+    },
+    interrupt::software::SoftwareInterruptControl,
+    spi::master::{Config, Spi},
+    time::Rate,
+    timer::timg::TimerGroup,
+};
+use esp_println as _;
+use esp_println::println;
+use heapless::String;
+use pure_fat::{
+    Bpb, DirEntryParser, ParseEntryOutput, ReadClusterInfo, ReadFile, ReadFileInput,
+    ReadFileOutput, ReadFilePart, StateMachine as _,
+};
+use pure_mbr::GenericMbr;
+use pure_wav::{
+    ChunkInfo, GetMetaDataForI2s, GetMetaDataForI2sOutput, Header, ReadRequest, StateMachine as _,
+    WaveFile, parse_top_header,
+};
+use spi_sd_card::{BLOCK_SIZE, Disk, EmbassySharedSpiBus, SpiSdCard};
+use zerocopy::{transmute, transmute_ref};
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[esp_rtos::main]
+async fn main(spawner: Spawner) {
+    let _ = spawner;
+
+    let peripherals = esp_hal::init(Default::default());
+
+    // Needed for esp_rtos
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let software_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, software_interrupt.software_interrupt0);
+
+    let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(1024);
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+    let spi_bus = Mutex::<CriticalSectionRawMutex, _>::new(
+        Spi::new(peripherals.SPI2, Config::default())
+            .unwrap()
+            .with_sck(peripherals.GPIO7)
+            .with_mosi(peripherals.GPIO6)
+            .with_miso(peripherals.GPIO5)
+            .with_dma(peripherals.DMA_CH0)
+            .with_buffers(dma_rx_buf, dma_tx_buf)
+            .into_async(),
+    );
+
+    let cs = Output::new(peripherals.GPIO0, Level::High, OutputConfig::default());
+
+    let mut sd_card = SpiSdCard::new(
+        EmbassySharedSpiBus::new(&spi_bus),
+        cs,
+        Delay,
+        Config::default().with_frequency(Rate::from_khz(400)),
+        Config::default().with_frequency(Rate::from_mhz(25)),
+    );
+
+    let mut card = sd_card.init_card().await.unwrap();
+    info!("Got card");
+    let capacity = card.capacity().await.unwrap();
+    info!("Card capacity: {} B", capacity);
+
+    let mut first_sector = [Default::default(); 512];
+    card.read(0, &mut first_sector).await.unwrap();
+    let mbr: GenericMbr = transmute!(first_sector);
+    let partition = mbr
+        .partition_entries
+        .iter()
+        .find(|entry| !entry.is_empty())
+        .unwrap();
+
+    println!("Partition: {:?}", partition);
+
+    let mut start_sector = [Default::default(); size_of::<Bpb>()];
+    let partition_start = partition.start_sector() as u64 * 512;
+    card.read(partition_start, &mut start_sector).await.unwrap();
+    let bpb: Bpb = transmute!(start_sector);
+    // println!("BPB: {:#?}", bpb);
+    // println!("FAT Type: {:#?}", bpb.fat_type());
+    let mut cluster_number = bpb.root_dir_start_cluster();
+    let mut entry_index_within_cluster = 0;
+    let mut parser = DirEntryParser::default();
+    let mut block_address = None;
+    let mut block = [Default::default(); BLOCK_SIZE];
+    let (first_cluster_number, size) = loop {
+        if entry_index_within_cluster == bpb.bytes_per_cluster() / 32 {
+            let mut cluster_info_buffer = [Default::default(); Bpb::MAX_CLUSTER_INFO_SIZE];
+            let cluster_info = &mut cluster_info_buffer[..bpb.cluster_info_size()];
+            card.read(bpb.cluster_info_start(cluster_number), cluster_info)
+                .await
+                .unwrap();
+            match bpb.next_cluster_number(cluster_info).unwrap() {
+                Some(next_cluster_number) => {
+                    cluster_number = next_cluster_number;
+                    entry_index_within_cluster = 0;
+                }
+                None => break None,
+            }
+        }
+        let entry_address = bpb.cluster_start(cluster_number) + entry_index_within_cluster * 32;
+        let required_block_address = entry_address / BLOCK_SIZE as u64 * BLOCK_SIZE as u64;
+        if !block_address.is_some_and(|block_address| block_address == required_block_address) {
+            card.read(partition_start + required_block_address, &mut block)
+                .await
+                .unwrap();
+            block_address = Some(required_block_address);
+        };
+        let start = entry_address as usize % BLOCK_SIZE;
+        let entry = block[start..start + 32].try_into().unwrap();
+        match parser.parse_entry(entry).unwrap() {
+            ParseEntryOutput::KeepReadingToParseCurrentEntry(new_parser) => {
+                parser = new_parser;
+            }
+            ParseEntryOutput::KeepReadingToParseNextEntry(entry) => {
+                parser = Default::default();
+                if let Some(entry) = entry {
+                    // Max len of UTF-8 from UTF-16 of [u16; N] is [u8; N * 3]
+                    let name = String::<{ 255 * 3 }>::from_utf16(&entry.name);
+                    println!("Name: {:?}", name);
+                    if char::decode_utf16(entry.name).eq("audio.wav".chars().map(Ok)) {
+                        break Some((entry.first_cluster_number, entry.size));
+                    }
+                }
+            }
+            ParseEntryOutput::DoneReadingEntries => {
+                break None;
+            }
+        }
+        entry_index_within_cluster += 1;
+    }
+    .unwrap();
+
+    let mut get_meta_data = GetMetaDataForI2s::new();
+    let meta_data = loop {
+        let output = get_meta_data.output();
+        match output {
+            GetMetaDataForI2sOutput::Done(output) => break output,
+            GetMetaDataForI2sOutput::Read(ReadRequest { address, size }) => {
+                let mut buffer = [Default::default(); GetMetaDataForI2s::MAX_READ_LEN];
+                let mut read_file = ReadFile::new(&bpb, first_cluster_number, address, size);
+                loop {
+                    let output = read_file.output();
+                    match output {
+                        ReadFileOutput::Done => {
+                            break;
+                        }
+                        ReadFileOutput::ReadFilePart(ReadFilePart {
+                            address_in_buffer,
+                            address_in_partition,
+                            copy_len,
+                        }) => {
+                            card.read(
+                                partition_start + address_in_partition,
+                                &mut buffer[address_in_buffer..address_in_buffer + copy_len],
+                            )
+                            .await
+                            .unwrap();
+                            read_file.input(ReadFileInput::DoneReadingPart);
+                        }
+                        ReadFileOutput::ReadClusterInfo(ReadClusterInfo {
+                            address_in_partition,
+                            len,
+                        }) => {
+                            let mut info_buffer = [Default::default(); Bpb::MAX_CLUSTER_INFO_SIZE];
+                            let info_buffer = &mut info_buffer[..len];
+                            card.read(partition_start + address_in_partition, info_buffer)
+                                .await
+                                .unwrap();
+                            read_file.input(ReadFileInput::ReadClusterInfo(info_buffer));
+                        }
+                    }
+                }
+                get_meta_data.input(&buffer[..size as usize]);
+            }
+        }
+    }
+    .unwrap();
+    println!("audio.wav: {meta_data:#?}");
+
+    let i2s = I2s::new(
+        peripherals.I2S0,
+        peripherals.DMA_CH1,
+        i2s::master::Config::new_tdm_msb()
+            .with_sample_rate(Rate::from_hz(meta_data.fmt_data.n_samples_per_sec.get()))
+            .with_data_format(match meta_data.fmt_data.w_bits_per_sample.get() {
+                16 => DataFormat::Data16Channel16,
+                _ => todo!(),
+            }),
+    )
+    .unwrap()
+    .into_async();
+    let (_rx_buffer, _rx_descriptors, tx_buffer, tx_descriptors) =
+        dma_circular_buffers!(0, 16 * 1024);
+    let mut tx = i2s
+        .i2s_tx
+        .with_bclk(peripherals.GPIO2)
+        .with_dout(peripherals.GPIO1)
+        .with_ws(peripherals.GPIO3)
+        .build(tx_descriptors);
+    let mut transfer = tx.write_dma_circular_async(tx_buffer).unwrap();
+}
