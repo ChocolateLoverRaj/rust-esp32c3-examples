ba5f505f4f82d5baa01bea81838d9769919251aa
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-01-28 20:59:33 -0800

    Improve

--- ws2812_matrix/src/main.rs
+++ ws2812_matrix/src/main.rs
@@ -1,22 +1,21 @@
 #![no_std]
 #![no_main]
+use core::num::NonZero;
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use embedded_graphics::{
-    mono_font::{MonoTextStyle, iso_8859_16::FONT_4X6},
-    pixelcolor::Rgb888,
+    framebuffer::{Framebuffer, buffer_size},
+    mono_font::{MonoTextStyle, iso_8859_16::FONT_5X8},
+    pixelcolor::{Rgb888, raw::BigEndian},
     prelude::*,
-    primitives::{PrimitiveStyleBuilder, Rectangle},
-    text::{Baseline, Text, renderer::TextRenderer},
+    text::{Baseline, Text},
 };
 use esp_backtrace as _;
-use esp_hal::{
-    interrupt::software::SoftwareInterruptControl, rmt::Rmt, time::Rate, timer::timg::TimerGroup,
-};
-use esp_hal_smartled::{SmartLedsAdapter, buffer_size_async, smart_led_buffer};
+use esp_hal::{rmt::Rmt, time::Rate, timer::timg::TimerGroup};
+use esp_hal_smartled::{SmartLedsAdapterAsync, buffer_size_async, smart_led_buffer};
 use esp_println as _;
-use smart_leds_matrix::{SmartLedMatrix, layout::Rectangular};
+use smart_leds::{RGB, SmartLedsWriteAsync, brightness};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -28,40 +27,61 @@ async fn main(spawner: Spawner) {
 
     // Needed for esp_rtos
     let timg0 = TimerGroup::new(p.TIMG0);
-    let software_interrupt = SoftwareInterruptControl::new(p.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, software_interrupt.software_interrupt0);
+    #[cfg(target_arch = "riscv32")]
+    let software_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(p.SW_INTERRUPT);
+    esp_rtos::start(
+        timg0.timer0,
+        #[cfg(target_arch = "riscv32")]
+        software_interrupt.software_interrupt0,
+    );
+
     const N_LEDS: usize = 64;
-    let mut buffer = smart_led_buffer!(N_LEDS);
-    let leds_adapter = {
-        SmartLedsAdapter::new(
-            Rmt::new(p.RMT, Rate::from_mhz(80)).unwrap().channel0,
-            p.GPIO7,
+    let mut buffer = smart_led_buffer!(buffer_size_async(N_LEDS));
+    let mut leds_adapter = {
+        SmartLedsAdapterAsync::new(
+            Rmt::new(p.RMT, Rate::from_mhz(80))
+                .unwrap()
+                .into_async()
+                .channel0,
+            p.GPIO0,
             &mut buffer,
         )
     };
-    let mut matrix = SmartLedMatrix::<_, _, N_LEDS>::new(leds_adapter, Rectangular::new(8, 8));
+    let mut matrix =
+        Framebuffer::<Rgb888, _, BigEndian, 8, 8, { buffer_size::<Rgb888>(8, 8) }>::new();
 
-    matrix.set_brightness(5);
-    // Create a new character style
-    let style = MonoTextStyle::new(&FONT_4X6, Rgb888::RED);
-
+    let style = MonoTextStyle::new(&FONT_5X8, Rgb888::RED);
     let mut text = Text::with_baseline("3.14159265", Point::zero(), style, Baseline::Top);
     let pixels_to_scroll = text
         .bounding_box()
         .size
         .width
-        .checked_sub(matrix.bounding_box().size.width);
+        .checked_sub(matrix.bounding_box().size.width)
+        .and_then(NonZero::new);
     let mut scroll_x = 0_u32;
     loop {
         text.position.x = 0 - scroll_x as i32;
-        matrix.clear(Rgb888::BLACK);
+        matrix.clear(Rgb888::BLACK).unwrap();
         text.draw(&mut matrix).unwrap();
-        matrix.flush().unwrap();
+        leds_adapter
+            .write(brightness(
+                matrix
+                    .data()
+                    .as_chunks::<3>()
+                    .0
+                    .iter()
+                    .copied()
+                    .map(|[r, g, b]| RGB::new(r, g, b)),
+                5,
+            ))
+            .await
+            .unwrap();
         if let Some(pixels_to_scroll) = pixels_to_scroll {
             if scroll_x == 0 {
                 Timer::after(Duration::from_secs(1)).await;
                 scroll_x += 1;
-            } else if scroll_x == pixels_to_scroll {
+            } else if scroll_x == pixels_to_scroll.get() {
                 Timer::after(Duration::from_secs(1)).await;
                 scroll_x = 0;
             } else {
